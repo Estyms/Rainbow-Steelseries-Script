118a498078d7c60375b66f2f1c6072c51ca140ed
Evann Regnault <evann@pop-os.localdomain>
2020-05-13 00:32:51 +0200

    Added support for gaming mode

--- README.md
+++ README.md
@@ -7,8 +7,9 @@ A dumb script to get rainbow effect on MSI Laptop steelseries keyboards
  - MSI Laptop
 
 ## Usage
-> ./Keyboard-Rainbow.sh -s [Delay]  -i [Hue Increment]
+> ./Keyboard-Rainbow.sh -s [Delay]  -i [Hue Increment] -m [Mode] 
 
  - -s : Set the delay in Millisecond between color changes
- - -i : Set the Hue Incrementation [0-360].
+ - -i : Set the Hue Incrementation [0-360]
+ - -m : Set the mode [normal, gaming]
  - -h : Prints the help
--- Rainbow-Keyboard.sh
+++ Rainbow-Keyboard.sh
@@ -10,8 +10,8 @@ C=0
 LAST="FF0000"
 SLAST="FF0000"
 HUE_INCREASE="5"
-SPEED="0.1"
-
+SPEED="1"
+MODE="normal" #normal, gaming
 
 
 # A fonctionning function for HUE TO RGB
@@ -51,19 +51,29 @@ HUE2RGB() {
     C=$(($TB))
 }
 
-
+ERROR(){
+    echo "Please install msiklm : https://github.com/Gibtnix/MSIKLM"
+    exit 1
+}
 
 # Main process
 MAIN() {
-    while (true) 
-    do
-        #Setting the Current Color
-        CURRENT="$(printf %02X%02X%02X $A $B $C)"
-        if sudo msiklm 0x$CURRENT,0x$SLAST,0x$LAST ; then
+    if sudo msiklm $MODE; then
+        while (true) 
+        do
+            #Setting the Current Color
+            CURRENT="$(printf %02X%02X%02X $A $B $C)"
+            if [ "$MODE" = "gaming" ]; then
+                sudo msiklm 0x$CURRENT $MODE
+            elif [ "$MODE" = "normal" ]; then
+                sudo msiklm 0x$CURRENT,0x$SLAST,0x$CURRENT
+            else
+                ERROR
+            fi
             #Shift the color to the right
             LAST=$SLAST
             SLAST=$CURRENT
-            
+                
             #Some delay
             sleep $(echo "scale=2;$SPEED/1000" | bc)
             #Increasing the hue
@@ -71,21 +81,33 @@ MAIN() {
 
             #Processing the new color from the hue
             HUE2RGB
-        else
-            echo "Please install msiklm : https://github.com/Gibtnix/MSIKLM"
-            exit 1
-        fi
-    done
+        done
+    else
+        ERROR
+    fi
 }
 
 # Argument Parsing
-while getopts i:s:h o
+while getopts i:s:m:h o
     do	case "$o" in   
-	    i)	    HUE_INCREASE="$OPTARG";;
-	    s)	    SPEED="$OPTARG";;
-        h)      printf "\n-s VALUE : Set the delay between Color Change in milliseconds\n-i VALUE : Set the increment of the Hue [0-360]\n\n"
+	    i)	    HUE_INCREASE=$OPTARG
+                if [ $HUE_INCREASE -lt $((0)) -o $HUE_INCREASE -gt $((360)) ]; then
+                    printf "\nInvalid value.\nCorrect values : [0-360]\n\n"
+                    exit 1
+                fi;;
+	    s)	    SPEED=$OPTARG
+                if [ $SPEED -lt $((0)) ]; then
+                    printf "\nInvalid value.\nCorrect Value : Any positive integer\n\n"
+                    exit 1
+                fi;;
+        m)      MODE="$OPTARG"
+                if [ "$MODE" != "gaming" -a "$MODE" != "normal" ]; then
+                    printf "\nInvalid mode.\nCorrect modes : [normal, gaming]\n\n"
+                    exit 1
+                fi;;
+        h)      printf "\n-s VALUE : Set the delay between Color Change in milliseconds\n-i VALUE : Set the increment of the Hue [0-360]\n-m MODE : Set the mode in which you want your keyboard to be [normal, gaming]\n\n"
                 exit 0;;
-	    [?])	printf "\nUsage: $0 [-s SPEED] [-i VALUE] [-h]\n\n"
+	    [?])	printf "\nUsage: $0 [-s SPEED] [-i VALUE] [-m MODE] [-h]\n\n"
 		        exit 1;;
 	    esac
 done
